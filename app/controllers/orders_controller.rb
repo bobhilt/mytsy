4aacde8bacad730bc5db487aaed710f88ca179b0
Bob Hiltner <bobhilt@gmail.com>
2015-01-08 20:42:40 -0800

    Add Order History pages.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,22 +3,18 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   respond_to :html
 
-  def index
-    @orders = Order.all
-    respond_with(@orders)
+  def sales 
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
   end
-
-  def show
-    respond_with(@order)
+  
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
   end
-
+    
   def new
     @order = Order.new
     @order.buyer_id = current_user.id
     @listing = Listing.find(params[:listing_id])
-  end
-
-  def edit
   end
 
   def create
@@ -39,16 +35,6 @@ class OrdersController < ApplicationController
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
-  end
-
-  def update
-    @order.update(order_params)
-    respond_with(@order)
-  end
-
-  def destroy
-    @order.destroy
-    respond_with(@order)
   end
 
   private
